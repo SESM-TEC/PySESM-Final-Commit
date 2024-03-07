9de82dd7e03778a79a8805b349fbddcfb596ac7f
Pablo Alvarado-Moya <palvarado@itcr.ac.cr>
2024-03-07 09:10:30 -0600

    Agrega comentario a to_triu_matrix

--- PySESM/utils/linalg.py
+++ PySESM/utils/linalg.py
@@ -3,6 +3,23 @@ import torch
 import math
 
 def to_triu_matrix(non_zeros):
+    """ Fill an upper triangular matrix with the content of the column
+        vector non_zeros.  If the matrix has dimensions n x n, then the
+        provided vector must have n(n+1)/2 elements.
+
+        non_zeros must be a torch tensor of floats, as a column vector
+
+        The matrix is filled rowwise, so, if the vector is [1 2 3 4 5 6]'
+        then the final matrix will be
+
+        |1 2 3|
+        |0 4 5|
+        |0 0 6|
+    """
+
+    # Find the dimension of the matrix.  If N is the dimension of the
+    # given vector, the solution of n²+n-2N = 0 is the size of the matrix,
+    # which is given by (sqrt(1+8N)-1)/2, and we round it up.
     matrix_n = math.ceil(0.5*(math.sqrt(8*non_zeros.size()[0] + 1) - 1))
 
     matrix = torch.zeros((matrix_n, matrix_n))
