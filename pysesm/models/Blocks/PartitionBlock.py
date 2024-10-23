feat: Fixed normalization on Partition manger

pysesm/models/Blocks/PartitionBlock.py
@@ -43,7 +43,6 @@ class PartitionBlock:
         return torch.all(self.block_scope[0] <= point_x) and torch.all(point_x <= self.block_scope[1])
 
     def normalize(self):
-        tensor_X = torch.tensor(self.X)
+        tensor_X = torch.stack(self.X)
         eps = self.block_scope * 1e-6
-        # Cambiar torch min oor average?
-        self.normalized_X = ((tensor_X - torch.min(tensor_X)) / (self.block_scope + eps))
+        self.normalized_X = (tensor_X - self.block_scope[0])/self.block_scope[1]

pysesm/models/Blocks/UniformPartitionManager.py
@@ -52,6 +52,7 @@ class UniformPartitionManager(BlockManager):
         self.blocks = None
         self.X = None
         self.y = None
+        self._vectorized_normalization = np.vectorize(lambda x: x.normalize())
 
     def _find_block(self, X: torch.Tensor) -> Union[PartitionBlock, None]:
         # TODO: Find efficient way to find block
@@ -119,9 +120,6 @@ class UniformPartitionManager(BlockManager):
         x_n = norm_x - t
         return t, x_n
 
-    def _normalize_blocks(self):
-        map(lambda x: x.normalize(), self.blocks)
-
     def _map_points(self, X: torch.Tensor, y: np.ndarray):
         """
         Locate points in their respective sub-blocks.
@@ -139,7 +137,8 @@ class UniformPartitionManager(BlockManager):
         self._update_block_arrangement(X)
 
         self._map_points(X, y)
-        self._normalize_blocks()
+        self._vectorized_normalization(self.blocks)
+        print(self.blocks)
         self._configure_blocks()
 
     def retrieve_active_blocks(self):