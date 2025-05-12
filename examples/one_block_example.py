be200d185a24d95ba01a149d0c9be25806b20b75
Pablo Alvarado-Moya <palvaradomoya@gmail.com>
2025-05-11 18:09:15 -0600

    Forgotten ,

--- examples/one_block_example.py
+++ examples/one_block_example.py
@@ -142,7 +142,7 @@ experiment = {
     "dictionary_criterion": JensenShannonLossWrapper(), 
     "rho_epochs": 10,
     "mu_epochs": 10,
-    "model_epochs": 10000
+    "model_epochs": 10000,
     "dict_epochs": 10,
     "sparse_coding_epochs": 50,
     "psi": SurrogateFunctionEnum.GAUSSIAN,
