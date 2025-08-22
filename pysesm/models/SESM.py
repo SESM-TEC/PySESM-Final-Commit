96be9d9208974a0a45e9672450fa8c1937808725
Pablo Alvarado-Moya <palvarado@itcr.ac.cr>
2025-08-22 15:35:08 -0600

    Preparando siguiente paso: inicialización solo la primera vez (falta)

--- pysesm/models/SESM.py
+++ pysesm/models/SESM.py
@@ -374,8 +374,8 @@ class SESM(torch.nn.Module, ABC):
         # Ensure sparse coding layer is initialized with block's h
         if not hasattr(block.sparse_coding_layer, 'h') or block.sparse_coding_layer.h is None:
             raise RuntimeError(
-                f"Critical error: block.sparse_coding_layer.h of the {block.block_index}-th block"
-                "is no initialized yet."
+                f"Critical error: block.sparse_coding_layer.h of the {block.block_index}-th block "
+                "is not initialized yet."
             )
 
         # Ensure the target y have the proper dimensions
