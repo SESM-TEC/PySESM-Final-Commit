8a31649430e1e69b40307774d5f4b42478d7e6f3
Aaron Salas <aasalas@estudiantec.cr>
2025-08-31 14:54:37 -0600

    refactorizacion de la clase del modelo SVR

--- experiments/SVR/model.py
+++ experiments/SVR/model.py
@@ -2,6 +2,7 @@ import numpy as np
 import joblib
 from sklearn.svm import SVR as svr
 import torch
+from sklearn.preprocessing import StandardScaler
 
 
 class SVR:
@@ -9,7 +10,7 @@ class SVR:
     Una clase para encapsular un modelo Support Vector Regression (SVR) de scikit-learn.
     """
     def __init__(self, kernel, C, gamma, epsilon):
-
+        self.scaler=StandardScaler()
         self.model = svr(
             kernel = kernel, 
             C = C, 
@@ -17,31 +18,36 @@ class SVR:
             epsilon = epsilon) 
 
 
-    def fit(self, X: torch.tensor, y: torch.tensor):
-        print("\n Training SVR...")
-        X = X.detach().cpu().numpy()
-        y = y.detach().cpu().numpy()
-        self.model.fit(X, y)
-        print(f"Número de vectores de soporte: {len(self.model.support_vectors_)}")
-        
+  
     def predict(self, X: torch.tensor) -> np.ndarray:
+        self.scaler.transform(X)
         predictions = self.model.predict(X)
         return predictions
+
 
     def save(self, path: str = 'svr_model.joblib'):
         joblib.dump(self.model, path)
         print(f"Model saved as {path}")
 
+
     def load(self, path: str = 'svr_model.joblib'):
         self.model = joblib.load(path)
         print(f"Model loaded'{path}'")
 
-    def train(self, xtrain, ytrain):
-        self.fit(xtrain, ytrain)
 
+    def train(self, xtrain, ytrain):
+        xtrain = self.scaler.fit_transform(xtrain)
+        print("\n Training SVR...")
+        self.model.fit(xtrain, ytrain)
+        print(f"Número de vectores de soporte: {len(self.model.support_vectors_)}")
+        
         # GUARDAR MODELO
         path = "svr_model.pth"
         self.save(path)
+
+
+
+
 
     def test(self, xtest):
 
