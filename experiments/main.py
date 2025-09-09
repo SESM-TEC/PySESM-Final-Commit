5d2affe6d64959af1bd33aae2a63a371138ca899
ValdiviaM <hvaldivia@estudiantec.cr>
2025-09-09 14:43:34 -0600

    Add dimension sweep and stores it as a dict of dicts

--- experiments/main.py
+++ experiments/main.py
@@ -32,93 +32,99 @@ def main():
         return torch.sin(pi * x) / (pi * x) - torch.sin(pi * y) / (pi * y)
     
     def sinc_3d_function(X):
-        x, y, z = X[:, 0], X[:, 1], X[:, 2]
+        features=[X[:, feature] for feature in range(X.size(1))]
         pi = np.pi
-        return torch.sin(pi * x) / (pi * x) - torch.sin(pi * y) / (pi * y) + torch.sin(pi*z)/(pi*z)
+        out=0
+        for idx, feature in enumerate(features):
+            out=out+((-1)^idx)*torch.sin(pi*feature)/(pi*feature)
+
+        return out
     
-    n_dimensions=3
+    n_dimensions= 3 
+    all_metrics_dim={}
+    all_times_dim={}
 
-    svr_config = {"kernel": 'rbf', "C": 0.1, "gamma": 'auto', "epsilon": 0.1}
-    nn_config = {"epochs": 500, "lr": 0.01, "hidden_dim": 16, "input_d":n_dimensions}
-    pf_config = {"order": 5, "alpha": 0.01}
-    
-    sparse_coding_config = ISTAConfig(
-        epochs=200, alpha=0.1, lambd=0.00001,
-        step_size_method=StepSizeMethod.FROBENIUS,
-        power_iterations=10, n_functions=10,
-        criterion=torch.nn.MSELoss()
-    )
+    for dim in range(2,n_dimensions+1):
+        num_runs_per_set = 20
 
-    dict_config = GaussianDictConfig(
-        epochs=40, alpha=0.01,
-        criterion=JensenShannonLossWrapper(),
-        optimizer_factory=lambda params, lr: torch.optim.SGD(params, lr=lr, momentum=0.1),
-        mu_epochs=10, rho_epochs=10, split_mu_rho=True,
-        eig_range=[0.05, 0.2], mu_range=[-2.0, 2.0],
-    )
 
-    partition_config = UniformPartitionConfig(
-        T=2,
-        initial_bounds=torch.tensor([[-2 for i in range(n_dimensions)], [2 for i in range(n_dimensions)]], dtype=torch.float32),
-        activity_threshold=0, overlap_ratio=0.1
-    )
+        svr_config = {"kernel": 'rbf', "C": 0.1, "gamma": 'auto', "epsilon": 0.1}
+        nn_config = {"epochs": 500, "lr": 0.01, "hidden_dim": 16, "input_d":dim}
+        pf_config = {"order": 5, "alpha": 0.01}
+        
+        sparse_coding_config = ISTAConfig(
+            epochs=200, alpha=0.1, lambd=0.00001,
+            step_size_method=StepSizeMethod.FROBENIUS,
+            power_iterations=10, n_functions=10,
+            criterion=torch.nn.MSELoss()
+        )
 
-    ssesm_config = SSESMConfig(
-        n_features= n_dimensions, model_epochs=200,
-        sparse_coding_config=sparse_coding_config,
-        dict_config=dict_config, partition_config=partition_config,
-        log_interval=100, permutation_times=1
-    )
+        dict_config = GaussianDictConfig(
+            epochs=40, alpha=0.01,
+            criterion=JensenShannonLossWrapper(),
+            optimizer_factory=lambda params, lr: torch.optim.SGD(params, lr=lr, momentum=0.1),
+            mu_epochs=10, rho_epochs=10, split_mu_rho=True,
+            eig_range=[0.05, 0.2], mu_range=[-2.0, 2.0],
+        )
 
-    #TODO: el diccionario experiment1 está pidiendo n_samples, pero n_samples es un valor que varia a lo largo del experimento
-    n_samples = 16  
-    experiment1 = {
-        "config": ssesm_config, "hyp_set": 1, "n_samples": n_samples,
-        "seed": 45, "iter": 0,
-        "device_map": {
-            DeviceTarget.GLOBAL: "cpu",
-            DeviceTarget.SPARSE_CODING_LAYER: "cpu",
-            DeviceTarget.DICTIONARY_LAYER: "cpu",
-            DeviceTarget.PARTITION_MANAGER: "cpu"
+        partition_config = UniformPartitionConfig(
+            T=2,
+            initial_bounds=torch.tensor([[-2 for i in range(dim)], [2 for i in range(dim)]], dtype=torch.float32),
+            activity_threshold=0, overlap_ratio=0.1
+        )
+
+        ssesm_config = SSESMConfig(
+            n_features= dim, model_epochs=200,
+            sparse_coding_config=sparse_coding_config,
+            dict_config=dict_config, partition_config=partition_config,
+            log_interval=100, permutation_times=1
+        )
+
+        #TODO: el diccionario experiment1 está pidiendo n_samples, pero n_samples es un valor que varia a lo largo del experimento
+        n_samples = 16  
+        experiment1 = {
+            "config": ssesm_config, "hyp_set": 1, "n_samples": n_samples,
+            "seed": 45, "iter": 0,
+            "device_map": {
+                DeviceTarget.GLOBAL: "cpu",
+                DeviceTarget.SPARSE_CODING_LAYER: "cpu",
+                DeviceTarget.DICTIONARY_LAYER: "cpu",
+                DeviceTarget.PARTITION_MANAGER: "cpu"
+            }
         }
-    }
 
 
 
-    # Definir las métricas y tiempos una sola vez
-    METRICS = ["NN_MAE", "NN_MSE", "SVR_MAE", "SVR_MSE", 
-            "SESM_MAE", "SESM_MSE", "PF_MAE", "PF_MSE"]
+        # Definir las métricas y tiempos una sola vez
+        METRICS = ["NN_MAE", "NN_MSE", "SVR_MAE", "SVR_MSE", 
+                "SESM_MAE", "SESM_MSE", "PF_MAE", "PF_MSE"]
 
-    TIMES = ["svr_time", "nn_time", "sesm_time", "pf_time"]
-
-
-    def init_dict(keys):
-        """Inicializa un diccionario con listas vacías por clave."""
-        return {key: [] for key in keys}
+        TIMES = ["svr_time", "nn_time", "sesm_time", "pf_time"]
 
 
-    # 1) Diccionarios principales
-    all_metrics = init_dict(METRICS)
-    all_times   = init_dict(TIMES)
+        def init_dict(keys):
+            """Inicializa un diccionario con listas vacías por clave."""
+            return {key: [] for key in keys}
 
-    
-    # 2) Inicializar Weights & Biases una sola vez
-    wandb.init(
-        project="PySESM_experiments",
-        config={
-            "svr_config": svr_config,
-            "nn_config": nn_config,
-            "SESM_config": ssesm_config,
-            "PF_config": pf_config,
-            "num_runs_per_set": num_runs_per_set
-        }
-    )
-    
-    dimensiones = [2, 3, 4, 5]
-    num_runs_per_set = 20
-    n_samples = [8, 16, 32, 64, 128, 256, 512, 1024]
 
-    for dim in dimensiones:
+        # 1) Diccionarios principales
+        all_metrics = init_dict(METRICS)
+        all_times   = init_dict(TIMES)
+
+        
+        # 2) Inicializar Weights & Biases una sola vez
+        wandb.init(
+            project="PySESM_experiments",
+            config={
+                "svr_config": svr_config,
+                "nn_config": nn_config,
+                "SESM_config": ssesm_config,
+                "PF_config": pf_config,
+                "num_runs_per_set": num_runs_per_set
+            }
+        )
+        
+        n_samples = [8, 16, 32, 64, 128, 256, 512, 1024]    
         for n in n_samples:
 
             # Diccionarios temporales para este chunk
@@ -148,9 +154,11 @@ def main():
                 all_metrics[key].append(chunk_metrics[key])
             for key in TIMES:
                 all_times[key].append(chunk_times[key])
+        all_metrics_dim[dim]=all_metrics
+        all_times_dim[dim]=all_times
 
-    joblib.dump(all_metrics, "./plots/all_metrics.joblib")
-    joblib.dump(all_times, "./plots/all_times.joblib")
+    joblib.dump(all_metrics_dim, "./plots/all_metrics.joblib")
+    joblib.dump(all_times_dim, "./plots/all_times.joblib")
     joblib.dump(n_samples, "./plots/n_samples.joblib")
 
     wandb.finish()
