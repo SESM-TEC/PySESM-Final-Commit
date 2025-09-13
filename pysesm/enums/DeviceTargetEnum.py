670ba5d1cbafaebaca3f3056ec6ab6e1fdafece7
Pablo Alvarado-Moya <palvarado@itcr.ac.cr>
2025-09-13 14:19:38 -0600

    DeviceManager removed, moving from a centralized strategy into a localized one, to ensure missing consistencies

--- pysesm/enums/DeviceTargetEnum.py
+++
@@ -1,8 +0,0 @@
-from enum import Enum
-
-class DeviceTarget(Enum):
-    """Enum defining the various components that can have device assignments"""
-    GLOBAL = "global"
-    SPARSE_CODING_LAYER = "sparse_coding_layer"
-    DICTIONARY_LAYER = "dictionary_layer"
-    PARTITION_MANAGER = "partition_manager"
