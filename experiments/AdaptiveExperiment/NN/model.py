d56cd1e2066cbb79cace9af5ed9a32eaadbee91b
ValdiviaM <hvaldivia@estudiantec.cr>
2025-11-27 15:25:22 -0600

    Add AdaptiveExperiment

---
+++ experiments/AdaptiveExperiment/NN/model.py
@@ -0,0 +1,70 @@
+"""ANN para regresion"""
+import torch.nn as nn
+import torch
+import torch.optim as optim
+import time
+
+
+class NN(nn.Module):
+    def __init__(self, epochs, lr, hidden_dim, input_d):
+        super().__init__()
+  
+        self.epochs = epochs
+        self.lr = lr
+        self.layers = nn.Sequential(
+            nn.Linear(input_d, hidden_dim),
+            nn.Tanh(), 
+            nn.Linear(hidden_dim, hidden_dim),
+            nn.Tanh(), 
+            nn.Linear(hidden_dim, 1)
+        )
+
+    def forward(self, x: torch.Tensor):
+        return self.layers(x)
+    
+
+    def train_nn(self, xtrain, ytrain, xtest, ytest):
+
+        # ENTRENAMIENTO
+        criterion = nn.MSELoss()
+        optimizer = optim.Adam(self.parameters(), lr=self.lr)
+        print("\n Training NN...")
+
+        start_time = time.time()
+        for epoch in range(self.epochs):
+            self.train()
+
+            # 1. Vaciar los gradientes
+            optimizer.zero_grad()
+
+            # 2. Forward pass: pasar el tensor completo
+            predictions = self(xtrain)
+
+            # 3. Calcular la pérdida
+            loss = criterion(predictions, ytrain.unsqueeze(1))
+
+            # 4. Backward pass y actualización de pesos
+            loss.backward()
+            optimizer.step()
+            
+            # Evaluación (opcional pero recomendado)
+            self.eval()
+            with torch.no_grad():
+                test_loss = criterion(self(xtest), ytest.unsqueeze(1))
+            
+            if (epoch + 1) % 100 == 0:
+                print(f"Epoch [{epoch+1}/{self.epochs}], "
+                    f"mse_train: {loss.item():.6f}, "
+                    f"mse_val: {test_loss.item():.6f}")
+        end_time = time.time()
+
+        return end_time - start_time
+
+    def test(self, xtest):
+        print("\n Testing NN...")
+        # Evaluación
+        predictions = self.layers(xtest)
+        # Conversion a numpy
+        predictions = predictions.detach().cpu()
+        return predictions
+        
---
+++ experiments/AdaptiveExperiment/SVR/model.py
@@ -0,0 +1,49 @@
+import numpy as np
+import joblib
+from sklearn.svm import SVR as svr
+import torch
+import time
+
+
+class SVR:
+    """
+    Una clase para encapsular un modelo Support Vector Regression (SVR) de scikit-learn.
+    """
+    def __init__(self, kernel, C, gamma, epsilon):
+        self.model = svr(
+            kernel = kernel, 
+            C = C, 
+            gamma = gamma, 
+            epsilon = epsilon) 
+        self.path = "svr_model.joblib"
+
+  
+    def predict(self, X: torch.tensor) -> np.ndarray:
+        predictions = self.model.predict(X)
+        return predictions
+
+
+    def save(self):
+        joblib.dump(self.model, self.path)
+
+
+    def load(self):
+        self.model = joblib.load(self.path)
+
+
+    def train(self, xtrain, ytrain):
+        print("\n Training SVR...")
+
+        start_time = time.time()
+        self.model.fit(xtrain, ytrain)
+        end_time = time.time()
+        print(f"Número de vectores de soporte: {len(self.model.support_vectors_)}")
+        
+        return end_time - start_time
+
+
+    def test(self, xtest):
+        print("\n Testing SVR...")
+        ypred = self.predict(xtest)
+        ypred = torch.from_numpy(ypred)
+        return ypred
